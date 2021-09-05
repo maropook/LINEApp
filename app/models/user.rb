948027aad195c085064e81386a56ce0ea870d14e
maropook <ii440092@icloud.com>
2021-09-05 09:52:28 +0900

    デザイン変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :messages, dependent: :destroy
   has_many :entries, dependent: :destroy
+  mount_uploader :image, ImageUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_04_225136) do
+ActiveRecord::Schema.define(version: 2021_09_04_232224) do
 
   create_table "entries", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -46,6 +46,8 @@ ActiveRecord::Schema.define(version: 2021_09_04_225136) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
